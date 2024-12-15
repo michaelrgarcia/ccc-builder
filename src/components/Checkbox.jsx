6a68fe928da8eb42e8850daea85dc40f5d0cd0bf
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-12-14 16:51:38 -0800

    Modularize checkbox

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,37 +1,19 @@
 import PropTypes from "prop-types";
 
-import { useState } from "react";
-
 import "../styles/Checkbox.css";
 
-function Checkbox({ checked }) {
-  const [boxChecked, setBoxChecked] = useState(checked);
-
-  function toggleCheck() {
-    setBoxChecked(!boxChecked);
-  }
-
-  if (boxChecked) {
-    return (
-      <label className="checkbox-container">
-        Test
-        <input type="checkbox" checked={true} onChange={toggleCheck} />
-        <span className="checkmark"></span>
-      </label>
-    );
-  } else {
-    return (
-      <label className="checkbox-container">
-        Test
-        <input type="checkbox" checked={false} onChange={toggleCheck} />
-        <span className="checkmark"></span>
-      </label>
-    );
-  }
+function Checkbox({ checked, onChange }) {
+  return (
+    <div className="checkbox-container">
+      <input type="checkbox" checked={checked} onChange={onChange} />
+      <span className="checkmark" onClick={onChange}></span>
+    </div>
+  );
 }
 
 Checkbox.propTypes = {
-  checked: PropTypes.bool,
+  checked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default Checkbox;
