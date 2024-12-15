7a786700ae43f0b8153b51d5357b4557844d19a3
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-12-14 23:19:18 -0800

    Add val prop

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,12 +2,13 @@ import PropTypes from "prop-types";
 import { forwardRef } from "react";
 
 const Input = forwardRef((props, ref) => {
-  const { id, type, placeholder, changeHandler, clickHandler } = props;
+  const { id, type, val, placeholder, changeHandler, clickHandler } = props;
 
   return (
     <input
       id={id}
       type={type}
+      value={val}
       placeholder={placeholder}
       ref={ref}
       onChange={changeHandler}
@@ -21,6 +22,7 @@ Input.displayName = "Input";
 Input.propTypes = {
   id: PropTypes.string,
   type: PropTypes.string.isRequired,
+  val: PropTypes.string,
   placeholder: PropTypes.string,
   changeHandler: PropTypes.func.isRequired,
   clickHandler: PropTypes.func,
