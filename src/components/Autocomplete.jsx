0782850fe0349e3476b1655e96117173aa7fe2b7
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-12-14 20:18:05 -0800

    Add click off logic

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,40 +1,67 @@
 import PropTypes from "prop-types";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import "../styles/Autocomplete.css";
 import MagnifyingGlass from "../assets/magnify.svg";
 
 import CheckboxOption from "./CheckboxOption";
+import Input from "./Input";
 
 function Autocomplete({ options }) {
   const [searchQuery, setSearchQuery] = useState("");
   const [showOptions, setShowOptions] = useState(false);
 
+  const optionsRef = useRef();
+  const inputRef = useRef();
+
+  const renderedOptions = options.map(({ name }, index) => (
+    <CheckboxOption optText={name} key={index} />
+  ));
+
   function updateSearch(e) {
     setSearchQuery(e.target.value);
   }
 
-  function toggleOptions() {
-    setShowOptions(!showOptions);
+  function toggleOptions(e) {
+    if (
+      optionsRef.current &&
+      !optionsRef.current.contains(e.target) &&
+      inputRef.current &&
+      !inputRef.current.contains(e.target)
+    ) {
+      setShowOptions(false);
+    }
   }
+
+  useEffect(() => {
+    document.addEventListener("click", toggleOptions, true);
+
+    return () => {
+      document.removeEventListener("click", toggleOptions, true);
+    };
+  }, []);
 
   return (
     <div className="autocomplete-container">
       <div className="search-input">
         <img src={MagnifyingGlass} />
-        <input
+        <Input
+          id="universities"
           type="text"
           placeholder="Select an institution..."
-          onChange={updateSearch}
-          onClick={toggleOptions}
+          ref={inputRef}
+          changeHandler={updateSearch}
+          clickHandler={() => setShowOptions(true)}
         />
       </div>
-      <div className="autocomplete-options">
-        {options.map(({ name }, index) => (
-          <CheckboxOption optText={name} key={index} />
-        ))}
-      </div>
+      {showOptions ? (
+        <div className="autocomplete-options" ref={optionsRef}>
+          {renderedOptions}
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
