ed2f0a3d5343f1fb1efd68fa41bf43ba30a9ff69
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-12-16 15:44:09 -0800

    Add school list to staticAssistData

---
+++ src/utils/staticAssistData.js
@@ -0,0 +1,130 @@
+export const schools = [
+  {
+    name: "California Polytechnic University, Humboldt",
+    id: 115,
+  },
+  {
+    name: "California Polytechnic University, Pomona",
+    id: 75,
+  },
+  {
+    name: "California Polytechnic University, San Luis Obispo",
+    id: 11,
+  },
+  {
+    name: "California State University, Bakersfield",
+    id: 98,
+  },
+  {
+    name: "California State University, Channel Islands",
+    id: 143,
+  },
+  {
+    name: "California State University, Chico",
+    id: 141,
+  },
+  {
+    name: "California State University, Dominguez Hills",
+    id: 50,
+  },
+  {
+    name: "California State University, East Bay",
+    id: 21,
+  },
+  {
+    name: "California State University, Fresno",
+    id: 29,
+  },
+  {
+    name: "California State University, Fullerton",
+    id: 129,
+  },
+  {
+    name: "California State University, Long Beach",
+    id: 81,
+  },
+  {
+    name: "California State University, Los Angeles",
+    id: 76,
+  },
+  {
+    name: "California State University, Maritime Academy",
+    id: 1,
+  },
+  {
+    name: "California State University, Monterey Bay",
+    id: 12,
+  },
+  {
+    name: "California State University, Northridge",
+    id: 42,
+  },
+  {
+    name: "California State University, Sacramento",
+    id: 60,
+  },
+  {
+    name: "California State University, San Bernardino",
+    id: 85,
+  },
+  {
+    name: "California State University, San Marcos",
+    id: 23,
+  },
+  {
+    name: "California State University, Stanislaus",
+    id: 24,
+  },
+  {
+    name: "San Diego State University",
+    id: 26,
+  },
+  {
+    name: "San Francisco State University",
+    id: 116,
+  },
+  {
+    name: "San Jose State University",
+    id: 39,
+  },
+  {
+    name: "Sonoma State University",
+    id: 88,
+  },
+  {
+    name: "University of California, Berkeley",
+    id: 79,
+  },
+  {
+    name: "University of California, Davis",
+    id: 89,
+  },
+  {
+    name: "University of California, Irvine",
+    id: 120,
+  },
+  {
+    name: "University of California, Los Angeles",
+    id: 117,
+  },
+  {
+    name: "University of California, Merced",
+    id: 144,
+  },
+  {
+    name: "University of California, Riverside",
+    id: 46,
+  },
+  {
+    name: "University of California, San Diego",
+    id: 7,
+  },
+  {
+    name: "University of California, Santa Barbara",
+    id: 128,
+  },
+  {
+    name: "University of California, Santa Cruz",
+    id: 132,
+  },
+];
