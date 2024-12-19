f84d087b5146fa1c3f5a24135cdba503c0c5088d
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-12-18 20:36:46 -0800

    Finish major selection component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { schools } from "../utils/staticAssistData";
 
@@ -10,12 +10,56 @@ import Autocomplete from "./Autocomplete";
 // this component will get huge. treat each if statement body as
 // if it were a separate function. mental overhead is not needed
 
+const academicYear = 75; // will be able to change this later
+
 function App() {
   const [currentStage, setCurrentStage] = useState("school-select");
   const [planProgress, setPlanProgress] = useState(0);
 
+  const [error, setError] = useState(null);
+
   const [selectedSchools, setSelectedSchools] = useState([]);
+
+  const [majors, setMajors] = useState([]);
   const [selectedMajors, setSelectedMajors] = useState([]);
+
+  useEffect(() => {
+    async function getMajors() {
+      try {
+        const majorData = {};
+
+        await Promise.all(
+          selectedSchools.map(async ({ name, id }) => {
+            const endpoint = import.meta.env.VITE_ASSIST_SCHOOLS;
+            const response = await fetch(
+              `${endpoint}/major-data/${id}/${academicYear}`
+            );
+
+            if (!response.ok) {
+              throw new Error("Failed to fetch majors for ", name);
+            }
+
+            const data = await response.json();
+            majorData[id] = data;
+          })
+        );
+
+        setMajors(majorData);
+      } catch (err) {
+        console.error("Failed to fetch majors: ", err);
+
+        setError("Critical error fetching major data. Refresh the page.");
+      }
+    }
+
+    if (selectedSchools.length > 0) {
+      getMajors();
+    }
+  }, [selectedSchools]);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
 
   if (currentStage === "school-select") {
     return (
@@ -35,7 +79,11 @@ function App() {
           </div>
         </header>
         <main>
-          <Autocomplete options={schools} updateParent={setSelectedSchools} />
+          <Autocomplete
+            options={schools}
+            placeholderTxt="Select an institution..."
+            updateParent={setSelectedSchools}
+          />
           {selectedSchools.length > 0 ? (
             <button
               type="button"
@@ -71,21 +119,18 @@ function App() {
         <main>
           <div className="major-selects">
             {selectedSchools.map(({ name, id }) => {
-              const endpoint =
-                "https://7bd2zfvix4.execute-api.us-east-2.amazonaws.com/transition1/schools";
-
               return (
                 <div className="major-select" key={id}>
                   <p className="school-name">{name}</p>
                   <Autocomplete
-                    options={schools}
+                    options={majors[id] || []}
+                    placeholderTxt="Select a major..."
                     updateParent={setSelectedMajors}
                   />
                 </div>
               );
             })}
           </div>
-
           {selectedMajors.length > 0 ? (
             <button
               type="button"
