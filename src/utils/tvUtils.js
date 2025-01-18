e35a30d7ce0679be6c26acb1c213e1c244fbdc62
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-01-18 14:48:52 -0800

    Adjust transfervision code to cccbuilder
    Old rendering functions persist to help me attune to the different data set.

--- src/utils/tvUtils.js
+++ src/utils/tvUtils.js
@@ -1,17 +1,5 @@
 import { caliCCs } from "./staticAssistData";
 
-export function createProgressTracker(linksLength) {
-  let totalProcessed = 0;
-
-  const updateProgress = (processed) => {
-    totalProcessed += processed;
-    // this used to set the textcontent of something
-    // updateProgressTracker(totalProcessed, linksLength);
-  };
-
-  return updateProgress;
-}
-
 export function createClassLists(articulation) {
   if (articulation && articulation.result) {
     const { result } = articulation;
@@ -47,7 +35,7 @@ export function createClassLists(articulation) {
   }
 }
 
-export async function getArticulationParams(receivingId, majorKey, year) {
+export function getArticulationParams(receivingId, majorKey, year) {
   const articulationParams = [];
 
   caliCCs.forEach((college) => {
@@ -69,183 +57,10 @@ export async function getArticulationParams(receivingId, majorKey, year) {
   return articulationParams;
 }
 
-async function processStream(stream, updateProgress) {
-  const reader = stream.getReader();
-  const decoder = new TextDecoder("utf-8");
-
-  const streamArticulations = [];
-  let accumulatedData = "";
-
-  while (true) {
-    const { value, done } = await reader.read();
-
-    if (done) {
-      break;
-    }
-
-    const decodedChunk = decoder.decode(value, { stream: true });
-    accumulatedData += decodedChunk;
-
-    let end = accumulatedData.indexOf("\n");
-
-    while (end !== -1) {
-      const jsonString = accumulatedData.slice(0, end);
-      accumulatedData = accumulatedData.slice(end + 1);
-
-      try {
-        const articulation = JSON.parse(jsonString);
-
-        if (articulation.result) {
-          createClassLists(articulation);
-          streamArticulations.push(articulation);
-        }
-
-        updateProgress(1);
-      } catch (error) {
-        console.error(`error parsing articulation: ${error}`);
-      }
-
-      end = accumulatedData.indexOf("\n");
-    }
-  }
-
-  return streamArticulations;
-}
-
-async function requestArticulations(
-  links,
-  signal,
-  courseId,
-  year,
-  updateProgress
-) {
-  let streamArticulations;
-
-  try {
-    const linksList = JSON.stringify(links);
-
-    const endpoint = `${
-      import.meta.env.VITE_NEW_ARTICULATION_FETCHER
-    }/?courseId=${courseId}&year=${year}`;
-
-    const response = await fetch(endpoint, {
-      body: linksList,
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Connection: "keep-alive",
-      },
-      signal,
-    });
-
-    streamArticulations = await processStream(response.body, updateProgress);
-  } catch (error) {
-    console.error(`error processing stream: ${error}`);
-  }
-
-  return streamArticulations;
-}
-
-async function processChunks(
-  processingQueue,
-  signal,
-  courseId,
-  updateProgress,
-  year,
-  assistArticulations
-) {
-  const concurrencyLimit = 29;
-
-  let linksChunk;
-
-  if (processingQueue.length === 0) return assistArticulations;
-
-  if (processingQueue.length < concurrencyLimit) {
-    linksChunk = processingQueue.splice(0, processingQueue.length - 1);
-  } else {
-    linksChunk = processingQueue.splice(0, concurrencyLimit);
-  }
-
-  try {
-    const streamArticulations = await requestArticulations(
-      linksChunk,
-      signal,
-      courseId,
-      year,
-      updateProgress
-    );
-
-    assistArticulations.push(...streamArticulations);
-
-    return await processChunks(
-      processingQueue,
-      signal,
-      courseId,
-      updateProgress,
-      year,
-      assistArticulations
-    ); // recursive call
-  } catch (error) {
-    if (error.name === "AbortError") {
-      console.log("aborted request");
-    } else {
-      console.error("error processing chunk:", error);
-    }
-  }
-}
-
-export function createListFromDb(articulations, linksLength, updateProgress) {
-  for (let i = 0; i < articulations.length; i++) {
-    const articulation = articulations[i];
-
-    if (articulation) {
-      createClassLists(articulation);
-    }
-  }
-
-  updateProgress(linksLength);
-  // hideLoadingContainer();
-}
-
-async function getClassFromDb(fullCourseId, linksLength, updateProgress) {
-  const courseGrabber = import.meta.env.VITE_COURSE_GRABBER;
-
-  try {
-    const response = await fetch(`${courseGrabber}/${fullCourseId}`);
-
-    if (response.status === 200) {
-      const articulations = await response.json();
-
-      createListFromDb(articulations, linksLength, updateProgress);
-
-      return articulations;
-    }
-
-    if (response.status === 204) {
-      console.log("restarting incomplete caching job...");
-
-      return false;
-    }
-
-    if (response.status === 206) {
-      // hideLoadingContainer();
-      // processingPrompt();
-
-      return true;
-    }
-  } catch (err) {
-    console.error("error getting class from db", err);
-  }
-
-  return false;
-}
-
-async function finalizeSearch(fullCourseId, articulations) {
+export async function finalizeSearch(fullCourseId, articulations) {
   const cacheFinalizer = import.meta.env.VITE_CACHE_COMPLETER;
 
   if (articulations) {
-    // organizeArticulations();
-
     try {
       await fetch(cacheFinalizer, {
         body: JSON.stringify({
@@ -263,52 +78,30 @@ async function finalizeSearch(fullCourseId, articulations) {
   }
 }
 
-export async function getArticulationData(links, courseId, year) {
-  const fullCourseId = `${courseId}_${year}`;
-  const processingQueue = links.slice();
-
-  const abortController = new AbortController();
-  const { signal, isAborted } = abortHandler(abortController);
-
-  const updateProgress = createProgressTracker(links.length);
-
-  let articulations;
-
-  window.addEventListener("beforeunload", () => abortController.abort());
-
-  updateProgress(0);
+export async function getClassFromDb(fullCourseId) {
+  const courseGrabber = import.meta.env.VITE_COURSE_GRABBER;
 
   try {
-    articulations = await getClassFromDb(
-      fullCourseId,
-      links.length,
-      updateProgress
-    );
+    const response = await fetch(`${courseGrabber}/${fullCourseId}`);
 
-    if (!articulations) {
-      articulations = await processChunks(
-        processingQueue,
-        signal,
-        courseId,
-        updateProgress,
-        year
-      );
+    if (response.status === 200) {
+      const articulations = await response.json();
 
-      // hideLoadingContainer();
-
-      if (!isAborted) {
-        await finalizeSearch(fullCourseId, articulations);
-      }
+      return articulations;
     }
-  } catch (error) {
-    if (error.name === "AbortError") {
-      console.log("requests aborted due to page unload");
-    } else {
-      console.error("error processing requests", error);
+
+    if (response.status === 204) {
+      console.log("restarting incomplete caching job...");
+
+      return false;
     }
-  } finally {
-    window.removeEventListener("beforeunload", () => abortController.abort());
+
+    if (response.status === 206) {
+      return true;
+    }
+  } catch (err) {
+    console.error("error getting class from db", err);
   }
 
-  return { articulations, updateProgress };
+  return false;
 }
