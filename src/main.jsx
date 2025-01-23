081607199fbd8b5005b794530a7f927716e5b36d
Michael Garcia <134650278+michaelrgarcia@users.noreply.github.com>
2025-01-23 00:50:30 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
+import { inject } from '@vercel/analytics';
+ 
+
 import "./index.css";
 import App from "./components/App.jsx";
 
+inject();
+
 createRoot(document.getElementById("root")).render(
-  <>
+
     <App />
-    <Analytics />
-  </>
+
 );
